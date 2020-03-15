73fa3b6818bdaa0d04c9a54734284844004bca07
Diego da Silva Pereira <diego.pereira@tema.group>
2020-03-15 01:23:41 -0300

    Remove semicolons

--- fipe/model.js
+++ fipe/model.js
@@ -1,15 +1,15 @@
 module.exports = class Model {
   constructor(args, brand) {
-    this.name = args.Label;
-    this.code = args.Value;
-    this.brand = brand;
+    this.name = args.Label
+    this.code = args.Value
+    this.brand = brand
   }
 
   set code(val) {
-    this._code = parseInt(val);
+    this._code = parseInt(val)
   }
 
   get code() {
-    return this._code;
+    return this._code
   }
 }
